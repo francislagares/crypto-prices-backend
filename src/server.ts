a0ba67ccac1ef932e3f76967cc57d3ff9e639dae
Francis Lagares <francis.lagares@gmail.com>
2022-02-20 20:57:31 +0100

    feat: send prices data through websockets

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import axios from 'axios';
 import * as dotenv from 'dotenv';
+import { IPrice } from './types/IPrices';
 
 dotenv.config();
 
@@ -22,20 +23,27 @@ socketHandler.on('connection', socket => {
   socketHandler.emit('crypto', 'Hello Cryptos Client !');
 });
 
-axios
-  .get(process.env.CRYPTO_LIST_URL as string)
-  .then(response => {
-    const pricesList = response.data.data.map(item => {
-      return {
-        id: item.id,
-        name: item.symbol,
-        price: item.metrics.market_data.price_usd,
-      };
+const getPrices = () => {
+  axios
+    .get(process.env.CRYPTO_LIST_URL as string)
+    .then(response => {
+      const pricesList = response.data.data.map((item: IPrice) => {
+        return {
+          id: item.id,
+          name: item.symbol,
+          price: item.metrics.market_data.price_usd,
+        };
+      });
+
+      socketHandler.emit('crypto', pricesList);
+    })
+    .catch(err => {
+      console.log(err);
     });
-    console.log(pricesList);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+};
+
+setInterval(() => {
+  getPrices();
+}, 5000);
 
 httpServer.listen(process.env.PORT);
