3fc02d0ca0e024f3795716dd1c7588f4e6527309
Francis Lagares <francis.lagares@gmail.com>
2022-02-20 20:37:52 +0100

    feat: fetch api prices data

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import { app } from './app';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const httpServer = createServer(app);
 const socketHandler = new Server(httpServer);
@@ -18,4 +22,20 @@ socketHandler.on('connection', socket => {
   socketHandler.emit('crypto', 'Hello Cryptos Client !');
 });
 
-httpServer.listen(3000);
+axios
+  .get(process.env.CRYPTO_LIST_URL as string)
+  .then(response => {
+    const pricesList = response.data.data.map(item => {
+      return {
+        id: item.id,
+        name: item.symbol,
+        price: item.metrics.market_data.price_usd,
+      };
+    });
+    console.log(pricesList);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+httpServer.listen(process.env.PORT);
